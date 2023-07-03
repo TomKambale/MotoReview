45f7f20dc59c4bbccfa7bf6491e51cd117e0012b
Ryan <ryanbiy01@gmail.com>
2023-07-04 01:39:31 +0300

    alignment

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
+        <h1 id="motto">Motorcycling at its best.</h1>
       <button id="reviewsbtn">Add Reviews</button>
       <button id="accountbtn">My Account</button>
     </nav>
